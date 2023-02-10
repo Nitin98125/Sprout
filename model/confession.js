4ad60ecfc3cee313da5491d0a75bffffe02441e1
Nitin98125 <66103181+Nitin98125@users.noreply.github.com>
2023-02-11 03:13:21 +0530

    Add files via upload

--- model/confession.js
+++ model/confession.js
@@ -1,25 +1,25 @@
 const { Schema, model } = require("mongoose");
 
-const ConfessSchema = Schema(
-  {
-    confession_no: {
-      type: Number,
-      required: true,
-    },
-    text: {
-      type: String,
-      required: true,
-    },
-    genre: {
-      type: String,
-      required: true,
-    },
-    emojis: {
-      type: Number,
-      required: true,
-    },
+const ConfessSchema = Schema({
+  confession_no: {
+    type: Number,
+    required: true,
   },
-  { timestamps: true }
-);
+  text: {
+    type: String,
+    required: true,
+  },
+  genre: {
+    type: String,
+    required: true,
+  },
+  emojis: {
+    type: Number,
+    required: true,
+  },
+  confessedOn:{
+    type:String,
+  }
+},{timeStamps:true});
 
 module.exports.Confession = model("Confession", ConfessSchema);
